790f81cb6cb402ebb6d749979c360eabaab0f036
belbanas <belbanas@gmail.com>
2020-12-18 15:57:45 +0100

    add images

--- src/components/PokemonItem.js
+++ src/components/PokemonItem.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react'
+import axios from "axios";
 
 export class PokemonItem extends Component {
+    state = {
+        image: null
+    }
+
+
+    componentDidMount() {
+        axios
+            .get(this.props.url)
+            .then(response => this.setState({ image: response.data.sprites.other.dream_world.front_default }))
+    }
+
+
     render() {
         return (
             <div>
+                <img src={this.state.image} alt="Pokemon's profile"/>
                 <p>
                     {this.props.name}
                     {this.props.key}
